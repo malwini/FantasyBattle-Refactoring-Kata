04f171d49ef967ce2302de0b91f9ad0e63c75084
Michael Dinitz <michaeldinitz@yahoo.de>
2023-04-04 20:01:16 +0200

    passing test, shitty code :)

--- go/player_test.go
+++ go/player_test.go
@@ -2,19 +2,65 @@ package codingdojo
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 	// "github.com/stretchr/testify/assert"
 )
 
+// | Where     | What            | Base Damage | Damage Modifier |
+// |-----------|-----------------|-------------|-----------------|
+// | right hand|  flashy sword of danger | 10  | 1.0             |
+// | right hand|  excalibur              | 20  | 1.5             |
+// | left hand |  round shield           |  0  | 1.4             |
+// | feet      |  ten league boots       |  0  | 0.1             |
+// | head      |  helmet of swiftness    |  0  | 1.2             |
+// | chest     |  breastplate of steel   |  0  | 1.4             |
+
 // choose this one if you are familiar with mocks
+type MockItem struct {
+}
+
+func (m MockItem) GetBaseDamage() int32 {
+	return 20
+}
+
+func (m MockItem) GetDamageModifier() float64 {
+	return 1.0
+}
+
+type MockBuff struct {
+}
+
+func (buff MockBuff) SoakModifier() float64 {
+	return 1.0
+}
+
+func (buff MockBuff) DamageModifier() float64 {
+	return 1.0
+}
+
+type MockArmor struct {
+}
+
+func (armor MockArmor) GetDamageSoak() int32 {
+	return 5
+}
+
 func TestDamageCalculationsWithMocks(t *testing.T) {
-	t.Skip("Test is not finished yet")
-	// inventory := mockInventory()
-	// stats := mockStats()
-	// target := mockSimpleEnemy()
 
-	// damage := MakePlayer(inventory, stats).CalculateDamage(target)
+	mockitem := MockItem{}
+	equipment := MakeEquipment(mockitem, mockitem, mockitem, mockitem, mockitem)
+	inventory := MakeInventory(equipment)
 
-	// assert.EqualValues(t, 10, damage.GetAmount())
+	// (equipment)
+	//inventory := mockInventory()
+	stats := MakeStats(10)
+	mockBuffs := []Buff{MockBuff{}}
+	target := MakeSimpleEnemy(MockArmor{}, mockBuffs)
+
+	damage := MakePlayer(inventory, stats).CalculateDamage(target)
+
+	assert.EqualValues(t, 590, damage.GetAmount())
 }
 
 // choose this one if you are not familiar with mocks
